449bf1498a004ccc1f02f906fa81b323cdb799a5
Alex Handley <smithvillescholarsah@gmail.com>
2024-08-02 21:59:39 -0500

    wip

--- migrations/20240803022136_initial-migration.js
+++ migrations/20240803022136_initial-migration.js
@@ -2,13 +2,17 @@
 exports.up = async function(knex) {
     await knex.schema
         .createTable('recipes', function(table) {
-            table.increments();
+            table.increments('recipe_id');
+            table.string('recipe_name', 128).notNullable().unique();
         })
         .createTable('ingredients', function(table) {
-            table.increments();
+            table.increments('ingredient_id');
+            table.string('ingredient_name', 128).notNullable().unique();
+            table.string('ingredient_unit', 64);
         })
         .createTable('steps', function(table) {
-            table.increments();
+            table.increments('step_id');
+            table.string('step_instructions')
         })
         .createTable('step_ingredients', function(table) {
             table.increments();
